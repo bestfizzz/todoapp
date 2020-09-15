a1b6781726ab8931e43709faf61930bb5a62a4fa
bestfizzz <52324074+bestfizzz@users.noreply.github.com>
2020-09-15 20:57:46 +0700

    h

---
+++ js/component.js
@@ -0,0 +1,56 @@
+const component={}
+component.registerPage=`
+<div class="register-container">
+<form id="register-form">
+    <div class="register-header">MindX chat</div>
+        <div class="name-wrapper">
+        <div class="input-wrapper">
+            <input type="text" placeholder="First name" name='firstName'>
+            <div class="error" id="first-name-error"></div>
+        </div>
+        <div class="input-wrapper">
+            <input type="text" placeholder="Last name" name="lastName">
+            <div class="error" id="last-name-error"></div>
+    </div>
+    </div>
+    <div class="input-wrapper">
+        <input type="email" placeholder='Email' name="email">
+        <div class="error" id="email-error"></div>    
+    </div>
+    <div class="input-wrapper">
+        <input type="password" placeholder="Password" name="password">
+        <div class="error" id="password-error"></div>
+    </div>
+    <div class="input-wrapper">
+        <input type="password" placeholder="Confirm password" name="confirmPassword">
+        <div class="error" id="confirm-password-error"></div>
+    </div>
+    <div class="form-action">
+        <div>Already have an account?<span class="cursor-pointer" id='redirect-to-login'>Login</span></div>
+        <button type="submit" class="btn cursor-pointer">register</button>
+    </div>
+</form>
+</div> 
+`
+component.loginPage=`
+<div class="login-container">
+<form id="login-form">
+    <div class="login-header">To Do List</div>
+    <div class="input-wrapper">
+        <input type="email" placeholder='Email' name="email">
+        <div class="error" id="email-error"></div>    
+    </div>
+    <div class="input-wrapper">
+        <input type="password" placeholder="Password" name="password">
+        <div class="error" id="password-error"></div>
+    </div>
+    <div class="form-action">
+        <div>Don't have an account?<span class="cursor-pointer" id='redirect-to-register'>Register</span></div>
+        <button type="submit" class="btn cursor-pointer">sign in</button>
+    </div>
+</form>
+</div>
+    `
+const toDoPage=`
+
+`
---
+++ js/index.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+    var firebaseConfig = {
+        apiKey: "AIzaSyDAtNk8VNMI3WMLHdJK5_R70ZHwhFkyqSg",
+        authDomain: "todoapp-576e8.firebaseapp.com",
+        databaseURL: "https://todoapp-576e8.firebaseio.com",
+        projectId: "todoapp-576e8",
+        storageBucket: "todoapp-576e8.appspot.com",
+        messagingSenderId: "1022767435245",
+        appId: "1:1022767435245:web:249f2eb891b87c5a83ebe4"
+    };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            model.currentUser = {
+                displayName: user.displayName,
+                email: user.email
+            }
+            if (user.emailVerified) {
+                view.setActiveScreen('chatPage')
+            } else {
+                alert('Pleas')
+                firebase.auth().signOut()
+                view.setActiveScreen('loginPage')
+            }
+        } else {
+            view.setActiveScreen('registerPage')
+        }
+    })
+    // templateFirestore()
+}
